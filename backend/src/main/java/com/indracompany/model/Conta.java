1fbaa2c165d5e38f866374eb6a7fc9f4686b77a2
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-06-16 11:34:28 -0300

    Mapeamento Objeto-Relacional

--- backend/src/main/java/com/indracompany/model/Conta.java
+++ backend/src/main/java/com/indracompany/model/Conta.java
@@ -1,20 +1,141 @@
 package com.indracompany.model;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.indracompany.enums.TipoConta;
 
-public class Conta {
+@Entity
+@Table(name = "contas")
+public class Conta implements Serializable {
+	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotNull
 	private String agencia;
 	
+	@NotNull
 	private String numero;
 	
+	@NotNull
 	private BigDecimal saldo;
 	
+	@NotNull
+	@Enumerated(EnumType.STRING)
 	private TipoConta tipo;	
 
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "conta")
+	private List<Transacao> transacoes = new ArrayList<>();
+	
+	public Conta() {}
+
+	public Conta(Long id, String agencia, String numero, BigDecimal saldo, TipoConta tipo, Cliente cliente) {
+		this.id = id;
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = saldo;
+		this.tipo = tipo;
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+
+	public TipoConta getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoConta tipo) {
+		this.tipo = tipo;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public List<Transacao> getTransacoes() {
+		return transacoes;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
--- backend/src/main/java/com/indracompany/model/Transacao.java
+++ backend/src/main/java/com/indracompany/model/Transacao.java
@@ -1,20 +1,132 @@
 package com.indracompany.model;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import com.indracompany.enums.TipoTransacao;
 
-public class Transacao {
+@Entity
+@Table(name = "transacoes")
+public class Transacao implements Serializable {
+	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant dataTransacao;
 	
+	@NotNull
+	@Enumerated(EnumType.STRING)
 	private TipoTransacao tipo;
 	
-	private Double valor;
+	@NotNull
+	private BigDecimal valor;
 	
 	private String informacoes;
 	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "conta_id")
 	private Conta conta;
+
+	public Transacao() {}
+
+	public Transacao(Long id, Instant dataTransacao, TipoTransacao tipo, BigDecimal valor, String informacoes, Conta conta) {
+		this.id = id;
+		this.dataTransacao = dataTransacao;
+		this.tipo = tipo;
+		this.valor = valor;
+		this.informacoes = informacoes;
+		this.conta = conta;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getDataTransacao() {
+		return dataTransacao;
+	}
+
+	public void setDataTransacao(Instant dataTransacao) {
+		this.dataTransacao = dataTransacao;
+	}
+
+	public TipoTransacao getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoTransacao tipo) {
+		this.tipo = tipo;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+
+	public String getInformacoes() {
+		return informacoes;
+	}
+
+	public void setInformacoes(String informacoes) {
+		this.informacoes = informacoes;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transacao other = (Transacao) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
